32fac0ac36aff8a484447853cdd513f060a0f280
VoiciehBy <43652981+VoiciehBy@users.noreply.github.com>
2026-01-07 01:44:02 +0100

    Multidigital update...
    - added support for multidigital operands
    - refactored code
    - etc.

--- src/main/java/edu/voiciehb/yetanothercalculator/MainController.java
+++ src/main/java/edu/voiciehb/yetanothercalculator/MainController.java
@@ -1,6 +1,7 @@
 package edu.voiciehb.yetanothercalculator;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class MainController {
@@ -9,6 +10,9 @@ public class MainController {
     private boolean isMinusButtonPressed = false;
     private boolean isStarButtonPressed = false;
     private boolean isSlashButtonPressed = false;
+    private boolean isEqualsButtonPressed = false;
+
+    private float result = 0.0f;
 
     private String sum(float value) {
         return String.valueOf(Float.parseFloat(resultLabel.getText()) + value);
@@ -21,56 +25,44 @@ public class MainController {
     }
     private String quotient(float value) {
         if(value != 0)
-        return String.valueOf(Float.parseFloat(resultLabel.getText()) / value);
+            return String.valueOf(Float.parseFloat(resultLabel.getText()) / value);
         else return "?";
     }
 
-    private void handleArithmeticOperations(float value){
+    private void handleArithmeticOperations(int value){
         if (isPlusButtonPressed) {
-            resultLabel.setText(sum(value));
+            result = Float.parseFloat(sum(value));
+            resultLabel.setText(String.valueOf(value));
             isPlusButtonPressed = false;
         } else if (isMinusButtonPressed) {
-            resultLabel.setText(difference(value));
+            result = Float.parseFloat(difference(value));
+            resultLabel.setText(String.valueOf(value));
             isMinusButtonPressed = false;
         } else if (isStarButtonPressed) {
-            resultLabel.setText(product(value));
+            result = Float.parseFloat(product(value));
+            resultLabel.setText(String.valueOf(value));
             isStarButtonPressed = false;
         }else if (isSlashButtonPressed) {
-            resultLabel.setText(quotient(value));
+            result = Float.parseFloat(quotient(value));
+            resultLabel.setText(String.valueOf(value));
             isSlashButtonPressed = false;
         }
-        else resultLabel.setText(String.valueOf(value));
+        else if(isEqualsButtonPressed){
+            resultLabel.setText(String.valueOf(result));
+            isEqualsButtonPressed = false;
+        }
+        else {
+            String tmp = resultLabel.getText();
+            if(tmp.equals("0"))
+                resultLabel.setText("");
+            resultLabel.setText(resultLabel.getText() + String.valueOf(value));
+        }
     }
 
-    public void oneButtonOnAction(ActionEvent actionEvent) {
-        handleArithmeticOperations(1);
-    }
-    public void twoButtonOnAction(ActionEvent actionEvent) {
-        handleArithmeticOperations(2);
-    }
-    public void threeButtonOnAction(ActionEvent actionEvent) {
-        handleArithmeticOperations(3);
-    }
-    public void fourButtonOnAction(ActionEvent actionEvent) {
-        handleArithmeticOperations(4);
-    }
-    public void fiveButtonOnAction(ActionEvent actionEvent) {
-        handleArithmeticOperations(5);
-    }
-    public void sixButtonOnAction(ActionEvent actionEvent) {
-        handleArithmeticOperations(6);
-    }
-    public void sevenButtonOnAction(ActionEvent actionEvent) {
-        handleArithmeticOperations(7);
-    }
-    public void eightButtonOnAction(ActionEvent actionEvent) {
-        handleArithmeticOperations(8);
-    }
-    public void nineButtonOnAction(ActionEvent actionEvent) {
-        handleArithmeticOperations(9);
-    }
-    public void zeroButtonOnAction(ActionEvent actionEvent) {
-        handleArithmeticOperations(0);
+    public void onDigitButtonOnAction(ActionEvent actionEvent){
+        Button button = (Button) actionEvent.getSource();
+        int digit = Integer.parseInt(button.getText());
+        handleArithmeticOperations(digit);
     }
 
     public void plusButtonOnAction(ActionEvent actionEvent) {
@@ -86,6 +78,8 @@ public class MainController {
         isSlashButtonPressed = true;
     }
     public void equalButtonOnAction(ActionEvent actionEvent) {
-        resultLabel.setText(resultLabel.getText());
+        isEqualsButtonPressed = true;
+        resultLabel.setText(String.valueOf(result));
+        isEqualsButtonPressed = false;
     }
 }
